c65cbcf8a01d966bfc3ef47bb647d421ce5cc433
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2021-03-09 15:16:38 +0200

    Add code to deal with race in install plans
    In some cases, the install plans might have a race and two will be
    created... one of the install plans will fail as the other one
    succeeded. In order to deal with this, we re-install.
    
    Signed-off-by: Juan Antonio Osorio Robles <jaosorior@redhat.com>

--- tests/e2e/helpers.go
+++ tests/e2e/helpers.go
@@ -18,6 +18,54 @@ const defaultOCLongWaitTimeout = "--timeout=10m"
 const scanDoneTimeout = 5 * time.Minute
 const defaultSleep = 5 * time.Second
 
+func ensureOperator() {
+	By("Deploying the Compliance Operator")
+	ocApplyFromString(`---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: openshift-compliance
+`)
+
+	ocApplyFromString(`---
+apiVersion: operators.coreos.com/v1alpha1
+kind: CatalogSource
+metadata:
+  name: compliance-operator
+  namespace: openshift-marketplace
+spec:
+  displayName: Compliance Operator Upstream
+  publisher: github.com/openshift/compliance-operator
+  sourceType: grpc
+  image: quay.io/compliance-operator/compliance-operator-index:latest
+
+`)
+
+	ocApplyFromString(`---
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: compliance-operator-sub
+  namespace: openshift-compliance
+spec:
+  channel: alpha
+  name: compliance-operator
+  source: compliance-operator
+  sourceNamespace: openshift-marketplace
+`)
+
+	ocApplyFromString(`---
+apiVersion: operators.coreos.com/v1
+kind: OperatorGroup
+metadata:
+  name: compliance-operator
+  namespace: openshift-compliance
+spec:
+  targetNamespaces:
+  - openshift-compliance
+`)
+}
+
 func do(cmd string, args ...string) string {
 	execcmd := exec.Command(cmd, args...)
 	output, err := execcmd.CombinedOutput()
