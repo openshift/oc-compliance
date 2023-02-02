8532be60cff152295506d1aa4edf550c1178f1ae
Jakub Hrozek <jhrozek@redhat.com>
2023-02-02 22:01:10 +0100

    e2e: Match on more states during re-runs
    The test tried to match only on PENDING, but if the container under test
    was too fast, it would go into LAUNCHING without the test noticing.
    Let's match on more states to make the test more resilient.

--- tests/e2e/rerunnow_test.go
+++ tests/e2e/rerunnow_test.go
@@ -27,7 +27,7 @@ var _ = Describe("rerun-now", func() {
 			By("checking the scan is re-kicked")
 			Eventually(func() string {
 				return oc("get", "compliancesuite", "rerun-now-scan", "-o", `jsonpath={.status.phase}`)
-			}, reRunTimeout).Should(ContainSubstring(`PENDING`))
+			}, reRunTimeout).Should(BeElementOf([]string{"PENDING", "LAUNCHING", "RUNNING"}))
 
 			By("waiting for scan to be done")
 			ocWaitLongFor("condition=ready", "compliancesuite", "rerun-now-scan")
